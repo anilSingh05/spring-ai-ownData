127b7eb36d9e54599a59b4453d65b0f93ac88594
Anil Singh <anil05.me@gmail.com>
2025-01-08 01:12:41 -0600

    Initial Commit

---
+++ src/main/java/spring_ai_ownData/SpringAiOwnDataApplication.java
@@ -0,0 +1,13 @@
+package spring_ai_ownData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOwnDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOwnDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_ai_ownData/SpringAiOwnDataApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_ai_ownData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOwnDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
